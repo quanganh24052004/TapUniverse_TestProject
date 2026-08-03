710369c2fc60e2d71add02c24bfc6d2236a08454
Nguyen Quang Anh <anh.nguyenquang24052004@gmail.com>
2026-08-03 09:41:41 +0700

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by quanganh on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
